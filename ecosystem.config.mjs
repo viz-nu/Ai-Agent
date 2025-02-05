5d79fd06d23970b65a67210647ea2b680a1732e4
viz-nu <vishnu.teja101.vt@gmail.com>
2025-02-06 00:33:41 +0530

    last test

--- ecosystem.config.mjs
+++ ecosystem.config.mjs
@@ -2,7 +2,7 @@ export default {
   apps: [
     {
       name: "my-app",
-      script: "src/index.js",
+      script: "./src/index.js",
       node_args: "-r dotenv/config",
       env: {
         NODE_ENV: "production",
