7aedcf01023c731cde7bbd4428ac07531a295295
viz-nu <vishnu.teja101.vt@gmail.com>
2025-02-20 00:00:45 +0530

    test

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,14 @@ from app.services.processing import crawl_and_store
 import asyncio
 main = Blueprint('main', __name__)
 
+@main.route('/', methods=['GET'])
+def test():
+    try:
+        
+        return jsonify({"test":"server up and running"})
+    except Exception as e:
+        # os.remove(file_path)  # Clean up temp file in case of error
+        return jsonify({"error": str(e)}), 500
 
 @main.route("/crawl-urls", methods=["POST"])
 def crawl_urls():
